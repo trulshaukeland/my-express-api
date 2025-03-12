af2896a40ff187a1567dae456282fc5f3f623dcf
Truls Agnar Haukeland <trulshaukeland@gmail.com>
2025-03-12 12:02:44 +0100

    Lagt til CORS og JSON-støtte

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Serveren kjører!");
